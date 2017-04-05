cc74707661124b04bd5f3789472fbe4af9f0d9a9
~P <houghstler@gmail.com>
2017-04-05 19:39:04 -0400

    Initial changes for user interface

--- proj1/tcpclient.c
+++ proj1/tcpclient.c
@@ -12,6 +12,18 @@
 
 #define STRING_SIZE 1024
 
+enum trans_type {
+    WITHDRAW,
+    DEPOSIT,
+    TRANSFER,
+    CHECK_BAL
+};
+
+enum acct_type {
+    CHECKING,
+    SAVINGS
+};
+
 int main(void) {
 
    int sock_client;  /* Socket used by client */
@@ -23,10 +35,39 @@ int main(void) {
    char server_hostname[STRING_SIZE]; /* Server's hostname */
    unsigned short server_port;  /* Port number used by server (remote port) */
 
-   char sentence[STRING_SIZE];  /* send message */
    char modifiedSentence[STRING_SIZE]; /* receive message */
    unsigned int msg_len;  /* length of message */                      
    int bytes_sent, bytes_recd; /* number of bytes sent or received */
+   char sentence[STRING_SIZE];  /* send message */
+   char choice[STRING_SIZE];
+   trans_type tchoice;
+   acct_type achoice;
+   int transtype, accttype, amount, balance_before, balance_after, err_code; // Special numbers for our server and client to interpret
+   
+/* Explanation Here for the ints below:   
+    Client:
+        withdraw:  0
+        deposit:   1
+        transfer:  2
+        check_bal: 3
+
+        checking:  0 
+        savings:   1
+
+        amount: [Natural #'s, -1]
+
+    Server response:
+        trans_type: [0,1,2,3]
+        account_type: [0,1]
+        balancebefore_trans:  [-1, Natural #'s]
+        balanceafter_trans: Integer
+        err_code: [0: no error, 1: insufficient funds, 2: withdrawal not multiple of 20, 3: attempting to withdraw from savings account]
+            4: transaction was > 1,000,000
+    
+    Client : { account_type, transaction_type, transaction_amount }   
+    Server : { account_type, trans_type, balancebefore_trans, balanceafter_trans, err_code }
+*/
+
   
    /* open a socket */
 
@@ -71,11 +112,52 @@ int main(void) {
       exit(1);
    }
   
-   /* user interface */
+    /* user interface */
+    // What action are you going to do?
+    printf("What action would you like to perform?\nWithdraw (w) | Deposit (d) | Transfer (t) | Check balance (b)\n>> ");
+    scanf("%s", choice);
+    switch (choice) {
+        case 'w':
+        transtype = WITHDRAW;
+        break;
 
-   printf("Please input a sentence:\n");
-   scanf("%s", sentence);
-   msg_len = strlen(sentence) + 1;
+        case 'd':
+        transtype = DEPOSIT;
+        break
+
+        case 't':
+        transtype = TRANSFER;
+        break;
+
+        case 'b':
+        transtype = CHECK_BAL;
+        break;
+
+        default: printf("You did not enter your response correctly. Please try again.");
+    }
+    // What account?
+    printf("What account are you doing this to?\nSavings (s) | Checking (c)\n>> ");
+    scanf("%s", choice);
+    switch (choice) {
+        case 's':
+        accttype = SAVINGS;
+        break;
+
+        case 'c': 
+        accttype = CHECKING;
+        break;
+
+        default: printf("You did not enter a valid response. Please try again.");
+    }
+
+    if(tchoice != CHECK_bal) {
+        // How much?
+        printf("How much money are you using?\n>> ");
+        scanf("%s", amount);
+    } else {
+        amount = -1;
+    }
+    msg_len = strlen(sentence) + 1;
 
    /* send message */
    
